f499474e957c5c385b17622b43c57d29cf4b4ca5
HASSANI KASSIM p1302141 <daroueche-kassim.hassani@etu.univ-lyon1.fr>
2018-01-08 11:48:20 +0100

    Ajout script parkings

--- utils/scrapping.py
+++ utils/scrapping.py
@@ -6,12 +6,17 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 uri = "https://data.rennesmetropole.fr/api/records/1.0/search/?rows=100&dataset=etat-des-stations-le-velo-star-en-temps-reel&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
+uriPark = "http://data.citedia.com/r1/parks/"
 sub_data = {}
 
 def createFile():
 	req = requests.get(uri, headers={'content-type': 'application/json'})
 	data = req.json()
+	reqPark = requests.get(uriPark, headers={'content-type': 'application/json'})
+	dataPark = reqPark.json()
+
 	records = data['records']
+	parks = dataPark['parks']
 	print("CREATE FILE")
 	print(datetime.now())
 	sub_data['nbJours'] = 7
@@ -29,12 +34,30 @@ def createFile():
 		sub_data['records'].append({'station_id' : id_station, 'nom' : nom, 'etat': [{date_time : taux_remplissage}]})
 		with open("historic.json", "w") as outfile:
 				json.dump(sub_data, outfile)
-	
+
+	for i in range (0, len(parks)):
+		ide = 100 + i
+		name = parks[i]['parkInformation']['name']
+		status = parks[i]['parkInformation']['status']
+		max_park = parks[i]['parkInformation']['max']
+		free=parks[i]['parkInformation']['free']
+		taux_remplissage = 0.0
+		if status == "AVAILABLE":
+			taux_remplissage = round(free / max_park * 100, 0)
+		sub_data['records'].append({'station_id' : ide, 'nom' : name, 'etat': [{date_time : taux_remplissage}]})
+		with open("historic.json", "w") as outfile:
+			json.dump(sub_data, outfile)
+
 
 def majFile():	
 	req = requests.get(uri, headers={'content-type': 'application/json'})
 	data = req.json()
 	records = data['records']
+
+	reqPark = requests.get(uriPark, headers={'content-type': 'application/json'})
+	dataPark = reqPark.json()
+	parks = dataPark['parks']
+
 	print("MAJ FILE")
 	print(datetime.now())
 	with open("historic.json", "r") as infile:
@@ -48,19 +71,34 @@ def majFile():
 		nom = fields['nom']
 		etat = fields['etat']
 		taux_remplissage = 0.0
-		if etat == "En fonctionnement":
+		if etat == "En fonctionnement":	
 			taux_remplissage = round(fields['nombrevelosdisponibles'] / fields['nombreemplacementsactuels'] * 100, 0)
 		sub_data['records'][i]['etat'].append({date_time: taux_remplissage})
-		
 		with open("historic.json", "w") as outfile:
 				json.dump(sub_data, outfile)
 
-	
+	j = 0
+	print(len(records))
+	for i in range (len(records) , 	len(sub_data['records']) - 1):
+		ide = 100+i
+		name = parks[j]['parkInformation']['name']
+		status = parks[j]['parkInformation']['status']
+		max_park = parks[j]['parkInformation']['max']
+		free=parks[j]['parkInformation']['free']
+		taux_remplissage = 0.0
+
+		if status == "AVAILABLE":
+			taux_remplissage = round(free / max_park * 100, 0)
+		
+		sub_data['records'][i]['etat'].append({date_time : taux_remplissage})
+		with open("historic.json", "w") as outfile:
+			json.dump(sub_data, outfile)	
+		j= j+1
 
 if __name__ == "__main__":
 	sched = BlockingScheduler()
-	sched.add_job(createFile, 'cron', month=1, day=1, hour=0, minute=0, second=0)
-	sched.add_job(majFile, 'interval', hours=1, start_date="2018-01-01 01:00:00", end_date="2018-01-08 00:00:00")
+	sched.add_job(createFile, 'cron', month=1, day=8, hour=12, minute=0, second=0)
+	sched.add_job(majFile, 'interval', hours=1, start_date="2018-01-08 13:00:00", end_date="2018-02-01 00:00:00")
 	print("DEBUT JOB")
 	print(datetime.now())
 	sched.start()
